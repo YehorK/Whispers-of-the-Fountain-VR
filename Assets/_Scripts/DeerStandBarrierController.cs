e66aa0872c8e07c07c472e00a08cfe87f946caa0
YehorK <yegorkarpichev@gmail.com>
2024-11-21 12:59:04 -0800

    minor fix

--- Assets/_Scripts/DeerStandBarrierController.cs
+++ Assets/_Scripts/DeerStandBarrierController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class BarrierController : MonoBehaviour
+public class DeerStandBarrierController : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
